ad7c9aeeca1e5a25b53e45addc12b01451e686bb
goddess <815484983@qq.com>
2019-12-26 17:24:34 +0800

    配置提交

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,28 @@
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 
+  <main-tab-bar></main-tab-bar>
+
 
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+  import MainTabBar from 'content/mainTabbar/MainTabBar'
 
 export default {
   name: 'app',
   components: {
     // HelloWorld
+    MainTabBar
   }
 }
 </script>
 
 <style>
 
-@import '@assets/css/base.css'
+/* @import '@assets/css/base.css' */
 
 
 </style>
